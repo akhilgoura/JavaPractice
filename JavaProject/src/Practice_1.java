5266483051994e8646f8656d889a24eb5b55b5be
AKHIL <akhil@AKHILs-MacBook-Air.local>
2022-05-01 20:39:24 +0530

    Updated the comment in Practice.java

--- JavaProject/src/Practice_1.java
+++ JavaProject/src/Practice_1.java
@@ -13,7 +13,7 @@ public class Practice_1 {
         float a = 7f/4f * 9f/2f;
         System.out.println(a);
 
-        // Encrypt a Grade
+        // Encrypt a Grade to a new value
         int grade;
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter the grade of the student: ");
